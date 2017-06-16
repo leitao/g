fe176d77aa589381393dacfc2d6c1ab6885d9cad
Breno Leitao <breno.leitao@gmail.com>
2017-06-16 14:10:14 -0300

    further changes

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-import hashlib       
+import hashlib
 import os
 import zlib
 
@@ -9,64 +9,62 @@ DEBUG = 1
 # $ openssl zlib -d -in <obj>
 
 def create_index(ci, filename, size):
-	index = os.getcwd() + "/.git/index"
+    index = os.getcwd() + "/.git/index"
 
-	fd = open(index, "w")
+    fd = open(index, "w")
 
-	buf = "10644 " + ci + " " + str(size) + "\t" + filename
-	if DEBUG:
-		print(buf)
+    buf = "10644 " + ci + " " + str(size) + "\t" + filename
+    if DEBUG:
+        print(buf)
 
-	sig = [ord('D'), ord('I'), ord('R'), ord('C')]
-	signature = bytearray(sig)
+    sig = [ord('D'), ord('I'), ord('R'), ord('C')]
+    signature = bytearray(sig)
 
-	ver = 0x2
-	
+    ver = 0x2
 
-	fd.write(buf)
-	fd.close()
-	
+    fd.write(buf)
+    fd.close()
 
 def compress(buf):
-	compressed = zlib.compress(bytes(buf.encode('utf-8')))
-	if 0:
-		print(compressed)
+    compressed = zlib.compress(bytes(buf.encode('utf-8')))
+    if 0:
+        print(compressed)
 
-	return compressed
+    return compressed
 
 
 # Create object into the database
 def create_obj(ci, buf):
-	head = ci[0:2]
-	tail = ci[2:]
+    head = ci[0:2]
+    tail = ci[2:]
 
-	if DEBUG:
-		print(head)
+    if DEBUG:
+        print(head)
 
-	objs = os.getcwd() + "/.git/objects/" + head + "/"
-	os.makedirs(objs, exist_ok=True)
+    objs = os.getcwd() + "/.git/objects/" + head + "/"
+    os.makedirs(objs, exist_ok=True)
 
 
-	objfile = open(objs + tail, "wb")
-	bytez = bytearray(compress(buf))
-	objfile.write(bytez)
-	objfile.close()
+    objfile = open(objs + tail, "wb")
+    bytez = bytearray(compress(buf))
+    objfile.write(bytez)
+    objfile.close()
 
 # Initial function to add a file into index
 def add(filename):
-	print("adding file", filename)
-	addfile = open(filename, 'r')
-	content = addfile.read() #.strip()
-	size = len(content)
-	buff = "blob " + str(size) + '\0' + content
-	print(buff)
-	sha_1 = hashlib.sha1()
-	sha_1.update(buff.encode('utf-8'))
+    print("adding file", filename)
+    addfile = open(filename, 'r')
+    content = addfile.read() #.strip()
+    size = len(content)
+    buff = "blob " + str(size) + '\0' + content
+    print(buff)
+    sha_1 = hashlib.sha1()
+    sha_1.update(buff.encode('utf-8'))
 
-	# commmit id
-	ci = sha_1.hexdigest()
-	if DEBUG:
-		print(ci)
-	
-	create_obj(ci, buff)
-	create_index(ci, filename, size)
+    # commmit id
+    ci = sha_1.hexdigest()
+    if DEBUG:
+        print(ci)
+
+    create_obj(ci, buff)
+    create_index(ci, filename, size)
--- g.py
+++ g.py
@@ -7,10 +7,18 @@ import init
 import add
 
 if __name__ == '__main__':
-	arg = sys.argv[1]
+    arg = sys.argv[1]
 
-	if arg == "init":
-		init.init();
-	if arg == "add":
-		filename = sys.argv[2]
-		add.add(filename);
+    if arg == "init":
+        init.init();
+    elif arg == "add":
+        filename = sys.argv[2]
+        add.add(filename);
+    elif arg == "diff":
+        print("diff: Diff files")
+    else:
+        print(sys.argv[0], "Usage:")
+        print("Options:\n")
+        print("\tinit: Initiate a bare repository")
+        print("\tadd: Add a file")
+        print("\tTBD")
